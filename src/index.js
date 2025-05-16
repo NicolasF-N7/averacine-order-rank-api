7c537204aee6718781129303e06c634434d45981
Nicolas perso <77533061+NicolasF-N7@users.noreply.github.com>
2025-05-16 17:43:57 +0200

    installed node-fetch

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
-const fetch = require('node-fetch'); // You'll need to install this: npm install node-fetch
+import fetch from 'node-fetch';
 
 app.use(express.urlencoded({ extended: false })); // To parse URL-encoded bodies
 
